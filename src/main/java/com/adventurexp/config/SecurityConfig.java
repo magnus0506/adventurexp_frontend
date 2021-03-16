421e9d8c2fbc4f966df53961b236d0f8d926dbaa
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2021-03-16 11:19:25 +0100

    a

---
+++ src/main/java/com/adventurexp/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.adventurexp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public DataSource dataSource(){
+        return DataSourceBuilder
+                .create()
+                .url("jdbc:mysql://54.234.57.19:3306/adventuredb?serverTimezone=UTC")
+                .username("root")
+                .password("password")
+                .build();
+    }
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource());
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
